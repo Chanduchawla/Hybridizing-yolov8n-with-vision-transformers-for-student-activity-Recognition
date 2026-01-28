d8bdeb8c0e21ee2a34a833bee131b244fb787057
Chanduchawla <103146019+Chanduchawla@users.noreply.github.com>
2026-01-28 16:49:26 +0530

    Update Roboflow_using_Yolo.py

--- Roboflow_using_Yolo/Roboflow_using_Yolo.py
+++ Roboflow_using_Yolo/Roboflow_using_Yolo.py
@@ -1,8 +1,5 @@
 import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+import pandas as pd # data processing 
 
 import os
 for dirname, _, filenames in os.walk('/kaggle/input'):
